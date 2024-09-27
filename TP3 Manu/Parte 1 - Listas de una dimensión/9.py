803770b8b6dda9a9da3eebb7575bb88919da75da
Manuel Palmero <manueligna06@gmail.com>
2024-09-26 23:41:16 -0300

    Cambios en el 9 Correción de una impresión por consola

--- TP3 Manu/Parte 1 - Listas de una dimensión/9.py
+++ TP3 Manu/Parte 1 - Listas de una dimensión/9.py
@@ -32,7 +32,7 @@ def filtrarPrimos(lista: list) -> list:
 lista = []
 
 while True:
-    n = input("Ingrese un elemento (presione S para terminar de ingresar números): ")
+    n = input("Ingrese un número (presione S para terminar de ingresar números): ")
     if n.upper() == "S":
         break
     lista.append(n)
