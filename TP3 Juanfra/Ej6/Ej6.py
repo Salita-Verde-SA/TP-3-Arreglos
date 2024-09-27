90379e7cec95598237dc0efac3ffe75639a11f03
FabrizioCastillo <Fabricastillo6@gmail.com>
2024-09-27 10:16:47 -0300

    FINALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL INDITINO

--- TP3 Juanfra/Ej6/Ej6.py
+++ TP3 Juanfra/Ej6/Ej6.py
@@ -1,9 +1,9 @@
-rta = "s"
+salida = "s"
 nums = []
-rta = input("Desea agregar un numero? S/N\n")
-while rta == "s" or rta == "S":
+salida = input("Desea agregar un numero? S/N\n")
+while salida == "s" or salida == "S":
     num = int(input("Ingrese el numero que desea agregar: "))
     nums.append(num)
-    rta = input("Desea agregar un numero? S/N\n")
+    salida = input("Desea agregar un numero? S/N\n")
 s = set(nums)
 print(f"El arreglo sin duplicados es: {s}")
